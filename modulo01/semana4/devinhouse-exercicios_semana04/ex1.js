961524ffef762d917a14e954835c0c9a8ddf1be2
Andre Knapik <andreluisknapik@gmail.com>
2022-04-05 14:40:00 -0300

    exercicio idade

---
+++ modulo01/semana4/devinhouse-exercicios_semana04/ex1.js
@@ -0,0 +1,7 @@
+let idade = 80
+
+if (idade < 16) {
+    console.log("Jovens - Indivíduos de até 15 anos")
+} else if (idade > 16 && idade < 64) {
+        console.log("Adultos - Indivíduos com idade entre 16 até 64 anos")  
+} else console.log("Idosos - Indivíduos de 65 anos em diante") 
